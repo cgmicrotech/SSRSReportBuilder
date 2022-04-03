5730235b1e3822f082c768cbb516adf3fe85ba91
Chris Green <Chris@cgmicrotech.com>
2022-04-03 14:44:24 -0500

    Can generate PDF reports with provided data sets.

--- ReportServer/Generators/RDLGenerator.cs
+++ ReportServer/Generators/RDLGenerator.cs
@@ -1,18 +1,74 @@
-﻿using Google.Protobuf;
+﻿using System.Data;
+using AspNetCore.Reporting;
+using Google.Protobuf;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using SSRSReportGeneratorServer;
+using DataSet = SSRSReportGeneratorServer.DataSet;
+using ReportResult = SSRSReportGeneratorServer.ReportResult;
 
 public class RDLGenerator
 {
-    public Task<ReportResult> GeneratePDF(ByteString rdlDefinition, DataSet data)
+    private string ReportDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Reports");
+
+    private IEnumerable<DataTable> CreateTablesFromDataSet(IEnumerable<DataSet> data)
     {
-        return Task.Run(() =>
+        var result = new List<DataTable>();
+        
+        foreach (var set in data)
         {
-            var rslt = new ReportResult()
+            var table = new DataTable(set.SetName);
+            IEnumerable<JObject> rows = JsonConvert.DeserializeObject<IEnumerable<JObject>>(set.SerializedData);
+            
+            
+            foreach (var row in rows)
             {
-                ReportType = ReportType.Pdf,
-                BinaryData = ByteString.CopyFrom(new byte[]{1})
-            };
-            return rslt;
-        });
+                var tblRow = table.NewRow();
+                foreach (var column in row.Properties())
+                {
+                    if (!table.Columns.Contains(column.Name))
+                        table.Columns.Add(column.Name, typeof(object));
+
+                    tblRow[column.Name] = row.GetValue(column.Name);
+                }
+
+                table.Rows.Add(tblRow);
+            }
+            result.Add(table);
+        }
+
+        return result;
+    }
+
+    public async Task<ReportResult> GeneratePDF(ByteString rdlDefinition, IEnumerable<DataSet> data)
+    {
+        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+        var fileNameGuid = Guid.NewGuid();
+        if (!Directory.Exists(ReportDirectory))
+            Directory.CreateDirectory(ReportDirectory);
+
+        var reportPath = Path.Join(ReportDirectory, $"{Guid.NewGuid()}.rdlc");
+        
+        await File.WriteAllBytesAsync(reportPath, rdlDefinition.ToByteArray());
+        var rpt = new LocalReport(reportPath);
+
+        var tableData = CreateTablesFromDataSet(data);
+
+        foreach (var table in tableData)
+            rpt.AddDataSource(table.TableName, table);
+
+        var binary = rpt.Execute(RenderType.Pdf);
+
+        var result = new ReportResult()
+        {
+            ReportType = ReportType.Pdf,
+            BinaryData = ByteString.CopyFrom(binary.MainStream),
+        };
+        
+        if(File.Exists(reportPath))
+            File.Delete(reportPath);
+        
+        return result;
     }
 }
