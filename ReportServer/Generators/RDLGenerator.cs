5ab7a9d374b0adc5168e8e78af6b9794b6092c0f
cgmicrotech <37902932+cgmicrotech@users.noreply.github.com>
2022-04-04 07:24:44 -0500

    Implement PDF report Generation (#1)
    * Created base function call
    
    * Can generate PDF reports with provided data sets.
    
    * Generate PDF from RDL
    
    * Set default configurations to listen on http2 only

--- ReportServer/Generators/RDLGenerator.cs
+++ ReportServer/Generators/RDLGenerator.cs
@@ -1,14 +1,74 @@
-﻿using Google.Protobuf;
-
-namespace ReportServer.Generators;
+﻿using System.Data;
+using AspNetCore.Reporting;
+using Google.Protobuf;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SSRSReportGeneratorServer;
+using DataSet = SSRSReportGeneratorServer.DataSet;
+using ReportResult = SSRSReportGeneratorServer.ReportResult;
 
 public class RDLGenerator
 {
-    public Task<ReportResult> GeneratePDF(ByteString rdlDefinition, DataSet data)
+    private string ReportDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Reports");
+
+    private IEnumerable<DataTable> CreateTablesFromDataSet(IEnumerable<DataSet> data)
     {
-        return new Task<ReportResult>(() => new ReportResult()
+        var result = new List<DataTable>();
+        
+        foreach (var set in data)
         {
-            ReportType = ReportType.Pdf
-        });
+            var table = new DataTable(set.SetName);
+            IEnumerable<JObject> rows = JsonConvert.DeserializeObject<IEnumerable<JObject>>(set.SerializedData);
+            
+            
+            foreach (var row in rows)
+            {
+                var tblRow = table.NewRow();
+                foreach (var column in row.Properties())
+                {
+                    if (!table.Columns.Contains(column.Name))
+                        table.Columns.Add(column.Name, typeof(string));
+
+                    tblRow[column.Name] = row.GetValue(column.Name);
+                }
+
+                table.Rows.Add(tblRow);
+            }
+            result.Add(table);
+        }
+
+        return result;
+    }
+
+    public async Task<ReportResult> GeneratePDF(ByteString rdlDefinition, IEnumerable<DataSet> data)
+    {
+        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+        var fileNameGuid = Guid.NewGuid();
+        if (!Directory.Exists(ReportDirectory))
+            Directory.CreateDirectory(ReportDirectory);
+
+        var reportPath = Path.Join(ReportDirectory, $"{Guid.NewGuid()}.rdlc");
+        
+        await File.WriteAllBytesAsync(reportPath, rdlDefinition.ToByteArray());
+        var rpt = new LocalReport(reportPath);
+
+        var tableData = CreateTablesFromDataSet(data);
+
+        foreach (var table in tableData)
+            rpt.AddDataSource(table.TableName, table);
+
+        var binary = rpt.Execute(RenderType.Pdf);
+
+        var result = new ReportResult()
+        {
+            ReportType = ReportType.Pdf,
+            BinaryData = ByteString.CopyFrom(binary.MainStream),
+        };
+        
+        if(File.Exists(reportPath))
+            File.Delete(reportPath);
+        
+        return result;
     }
 }
--- ReportServer/Services/ReportService.cs
+++ ReportServer/Services/ReportService.cs
@@ -1,10 +1,10 @@
 using Grpc.Core;
-using ReportServer;
-using ReportServer.Generators;
+using SSRSReportGeneratorServer;
+using static SSRSReportGeneratorServer.ReportServices;
 
 namespace ReportServer.Services
 {
-    public class ReportService : ReportServices.ReportServicesBase
+    public class ReportService : ReportServicesBase
     {
         private readonly ILogger<ReportService> _logger;
         public ReportService(ILogger<ReportService> logger)
@@ -12,20 +12,12 @@ namespace ReportServer.Services
             _logger = logger;
         }
 
-        public override Task<PrintResult> PrintReport(PrintRequest request, ServerCallContext context)
+        public override async Task<ReportResult> GenerateReport(ReportGenerationRequest request, ServerCallContext context)
         {
-            return base.PrintReport(request, context);
-        }
+            var result = new ReportResult();
 
-        public override Task<ReportResult> GenerateReport(ReportGenerationRequest request, ServerCallContext context)
-        {
-            return request.RepotType switch
-            {
-                ReportType.Pdf => new RDLGenerator().GeneratePDF(request.ReportDefinition, request.ReportData),
-                ReportType.Xls => throw new NotImplementedException(),
-                ReportType.Html => throw new NotImplementedException(),
-                _ => throw new InvalidDataException("Unable to determine a proper way to generate the chosen report type.")
-            };
+            result = await new RDLGenerator().GeneratePDF(request.ReportDefinition, request.ReportData);
+            return result;
         }
 
     }
